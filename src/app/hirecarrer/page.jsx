84114c20b582e0bf0c97a4142ff1d5f2b35d86e3
mrankit5575 <mrankit5575@gmail.com>
2025-07-18 17:27:22 +0530

    update

---
+++ src/app/hirecarrer/page.jsx
@@ -0,0 +1,12 @@
+import TutorsSection from '@/components/TutorSection'
+import React from 'react'
+
+function page() {
+  return (
+    <div className=''>
+        <TutorsSection className="mt-20"/>
+     </div>
+  )
+}
+
+export default page
