39ab299fc8a053d431f226296f002e55242b4dbf
mrankit5575 <mrankit5575@gmail.com>
2025-07-19 13:45:32 +0530

    udpate remvoe the all book

--- src/app/all-books/page.jsx
+++ src/app/all-books/page.jsx
@@ -1,13 +1,1 @@
-import BookCard from '@/bookdata.js/BookCard'
-import BookList from '@/bookdata.js/BookList'
-import React from 'react'
-
-function page() {
-  return (
-    <div>
-       <BookCard/>
-    </div>
-  )
-}
-
-export default page
+ 
